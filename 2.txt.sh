5e416ba65ca181f64690363b7a2375f1d7e7a3b8
jeramys-sys <jeramys@sas.upenn.edu>
2025-10-21 23:40:55 -0400

    Add files via upload

---
+++ 2.txt.sh
@@ -0,0 +1,19 @@
+p. 2
+On and on he went, over rocks and
+crags and roots of trees; and still the
+bear kept so far ahead, that there was no
+use in trying to shoot at it. At last, a
+good way up the mountain side, the bear
+suddenly disappeared. Coming to
+the place, the young hunter
+saw that it
+was the mouth of an immense cavern,
+dark and high, and with just
+a glimmer of
+light
+at the
+other end. Although
+he could no longer see the bear, he
+made sure that it had
+escaped through
+this cavern.
